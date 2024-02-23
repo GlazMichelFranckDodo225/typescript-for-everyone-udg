1c6796246d09de1a69ecf1aa15936167b132b5c7
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-23 16:16:13 +0100

    Var Const et Let

---
+++ b-var-const-let/script.js
@@ -0,0 +1,31 @@
+/* var age = 20;
+console.log(age);
+
+var age = 50;
+console.log(age); */
+/* let age = 20;
+console.log(age);
+
+let age = 50; // Error ==> Cannot redeclare block-scoped variable 'age' */
+/* const age = 20;
+console.log(age);
+
+const age = 50; // Error ==> Cannot redeclare block-scoped variable 'age' */
+// The Proper Usage of const and let
+// const
+var PI = Math.PI;
+// PI = 3.14; // Error ==> Cannot assign to 'PI' because it is a constant
+console.log(PI);
+var API_KEY = "Secret_key_215s33d422GhJK5214";
+// API_KEY = "Secret_key_54cv255451s111G" // Error ==> Cannot assign to 'PI' because it is a constant
+console.log(API_KEY);
+// const KEY; // Error ==> 'const' declarations must be initialized
+// let
+var number = 20;
+number = 50;
+console.log(number);
+number = 75;
+console.log(number);
+var firstName;
+firstName = "John";
+console.log(firstName);
